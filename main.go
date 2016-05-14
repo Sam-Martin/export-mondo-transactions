a77826031f13f3136e2b0d3fe720faf18ca37980
Sam Martin <samjackmartin@gmail.com>
2016-05-14 13:21:19 +0100

    Multiple transactions allowed

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ type Transaction struct{
 
 type OFX struct {
   XMLName     xml.Name `xml: "OFX"`
-  Transaction Transaction `xml:"SIGNONMSGSRSV1>BANKMSGSRSV1>SMTRS>BANKTRANLIST>STMTTRN"`
+  Transaction []Transaction `xml:"SIGNONMSGSRSV1>BANKMSGSRSV1>SMTRS>BANKTRANLIST>STMTTRN"`
 }
 
 func check(e error) {
@@ -37,13 +37,20 @@ func main() {
     dir, err := os.Getwd()
 
     v := &OFX{}
-    v.Transaction = Transaction{
+    v.Transaction = append(v.Transaction,Transaction{
       TRNTYPE: "POS",
       DTPOSTED: "20160408120000.000[+1]",
       TRNAMT: -8.49,
       FITID: "00POS201604081200000001-849HALFORDS0371MAIDENHEADGB0000",
       NAME: "HALFORDS 0371 MAIDENHEAD GB 0000",
-    }
+    })
+    v.Transaction = append(v.Transaction,Transaction{
+      TRNTYPE: "PO123123S",
+      DTPOSTED: "20160408120000.000[+1]",
+      TRNAMT: -8.49,
+      FITID: "00POS201604081200000001-849HALFORDS0371MAIDENHEADGB0000",
+      NAME: "HALFORDS 0371 MAIDENHEAD GB 0000",
+    })
 
     output, err := xml.MarshalIndent(v, "  ", "    ")
     check(err)
